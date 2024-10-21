f4fe1a09e84c979ba50fc341a64465ecba1593d7
eslahkonha.mona@gmail.com <eslahkonha.mona@gmail.com>
2024-10-21 16:13:02 +0200

    test solved

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor () {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add (item) {
+    this.items.push(item)
+    this.items = this.items.sort((a, b) => a - b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get (pos) {
+    if (pos > 0 && pos < this.items.length) {
+      return this.items[pos]
+    } else {
+      throw new Error('OutOfBounds')
+    }
+  }
 
-  max() {}
+  max () {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return this.items[this.items.length - 1]
+    }
+  }
 
-  min() {}
+  min () {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return this.items[0]
+    }
+  }
 
-  sum() {}
+  sum () {
+    return this.items.reduce((acc, num) => acc + num, 0)
+  }
 
-  avg() {}
+  avg () {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return this.sum() / this.items.length
+    }
+  }
 }
 
-module.exports = SortedList;
+module.exports = SortedList
